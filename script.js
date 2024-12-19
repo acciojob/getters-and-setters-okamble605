1cc4756df8ffe3ee5c880b0716128f9bf0ba3d7a
Omkar Baburao Kamble <121740559+okamble605@users.noreply.github.com>
2024-12-19 18:32:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-//complete this code
-class Person {}
+// Define the Person class
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  // Getter for name
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  // Setter for age
+  set age(newAge) {
+    this._age = newAge;
+  }
+}
+
+// Define the Student class extending Person
+class Student extends Person {
+  // Method for studying
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+// Define the Teacher class extending Person
+class Teacher extends Person {
+  // Method for teaching
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
